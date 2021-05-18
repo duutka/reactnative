f5e9596d657d1ce8fc62dee5a2d53f1c86213661
duutka <loj7mlbdf6l9@gmail.com>
2021-05-18 14:46:17 +0500

    screens and nav

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
-  arrowParens: 'avoid',
+  tabWidth: 4,
+  printWidth: 100,
+  useTabs: true,
 };
